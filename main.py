09dc3509e63a1362afeed0ee9dc2aa2c9f85cc62
Роман Шорников <126609619+roman-shornikov@users.noreply.github.com>
2023-04-26 05:12:09 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-# import math
-# print("Введите число")
-# a=int(input())
-# z1 = math.cos(a)+math.sin(a)+math.cos(3*a)+math.sin(3*a)
-# print("Первое значение")
-# print(z1)
-# z2 = 2*math.sqrt(2)*math.cos(a)*math.sin(math.pi/4+2*a)
-# print("Второе значение")
-# print(z2)
+# №2
+# string = "123456789"
+# print("Вывод символов при помощи положительных индексов ")
+# c3 = string[2]
+# print(c3)
+# c5 = string[4]
+# print(c5)
+# c567 = string[4:7]
+# print(c567)
+# print("Вывод символов при помощи отрицательных индексов ")
+# c23 = string[-7]
+# print(c23)
+# c25 = string[-5]
+# print(c25)
+# x = string[-5:-2]
+# print(x)
+# №3
+#
+# b = "кот"
+# print(b)
+# tt = b[2] + b[1] + b[0]
+# print(tt)
+#
+#
+# №4
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 5, 564, 456]
+# print(a[0])
+# print(a[-1])
 
-import math
-
-a=-math.pi/4
-b=math.pi/4
-n=15
-
-h=(b-a)/n
-while a<b:
-    F=math.sin(3*a)+4*a
-    print(f"a={a} F={F}")
-    a=a+h
+#
+# №5
+# def sum_range(start,end):
+#     sum=0
+#     if end < start:
+#         start, end = end, start
+#     for a in range(start,end,1):
+#         sum=sum+a
+#     return sum
+# start = int(input())
+# end =int(input())
+# print(sum_range(start,end))
